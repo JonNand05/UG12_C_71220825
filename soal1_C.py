6961dbd85519c9573b28014996e1d1bfc0dfc8c0
JonNand05 <95239194+JonNand05@users.noreply.github.com>
2022-12-08 16:33:44 +0700

    Add files via upload

---
+++ soal1_C.py
@@ -0,0 +1,13 @@
+kalimat =str(input("masukan kata atau angka:"))
+def reserve (kalimat):
+    str=""
+    for i in kalimat:
+        str = i + str
+    return str
+print (reserve(kalimat)) 
+
+
+
+
+
+
---
+++ soal2_C.py
@@ -0,0 +1,10 @@
+teks = input("masukan angka:")
+hitung = input("masukan angka yang dihitung:")
+
+
+sum = 0
+
+for each in teks :
+    if each in hitung:
+     sum = sum + 1
+print("Jumlah semuanya:", sum)
